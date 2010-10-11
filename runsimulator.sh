c277bed0cff1625caa6082c179b0c9e6056f2bcd
Roshan Shariff <roshan.shariff@ualberta.ca>
2010-10-10 21:29:12 -0600

    Send output to simulation_log.txt

--- runsimulator.sh
+++ runsimulator.sh
@@ -4,11 +4,23 @@ prefix=$1
 runs=100
 cores=2
 
+date >> "${prefix}simulation_log.txt"
+echo "Runs: $runs" >> "${prefix}simulation_log.txt"
+echo "Cores: $cores" >> "${prefix}simulation_log.txt"
+
 eval echo {1..$runs} | xargs -n 1 -P $cores -I XXX ./simulator -o "${prefix}runXXX."
 
 summary_files=$(eval echo \\\"\\\${prefix}run{1..$runs}.summary.txt\\\")
-echo $summary_files
-for i in "Elapsed time" "Landmark error" "State error"; do
-    echo -n "$i: "
-    eval sed -n "'s/$i: \(.*\)/\1/p'" $summary_files | average
+cut -d: -f1 "${prefix}run1.summary.txt" | while read name; do
+    echo -n "$name: " >> "${prefix}simulation_log.txt"
+    eval sed -n "'s/$name: \(.*\)/\1/p'" $summary_files \
+	| average >> "${prefix}simulation_log.txt"
+done
+echo >> "${prefix}simulation_log.txt"
+times >> "${prefix}simulation_log.txt"
+echo >> "${prefix}simulation_log.txt"
+
+for filename in landmarks{,.estimated} state{,.estimated,.expected} summary; do
+    files=$(eval echo \\\"\\\${prefix}run{1..$runs}.$filename.txt\\\")
+    eval rm $files
 done
