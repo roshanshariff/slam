de4645d0fc0f7b141d061c4569e8e5ccbdee55ce
Roshan Shariff <roshan.shariff@ualberta.ca>
2011-08-18 11:08:32 -0600

    Fixed whitespace and indenting

--- src/planar_robot/range_bearing_model.hpp
+++ src/planar_robot/range_bearing_model.hpp
@@ -10,59 +10,58 @@
 namespace planar_robot {
 
 
-  class range_bearing_model {
+class range_bearing_model {
 
-    normal_dist<double> range;
-    normal_dist<double> bearing;
+	normal_dist<double> range;
+	normal_dist<double> bearing;
 
-  public:
+public:
 
-    typedef position result_type;
+	typedef position result_type;
 
-    range_bearing_model () : range(1.0, 1.0), bearing(0.0, 1.0) { }
+	range_bearing_model () : range(1.0, 1.0), bearing(0.0, 1.0) { }
 
-    range_bearing_model (const position& mean,
-		   double range_sigma, double bearing_sigma)
-      : range (mean.range(), range_sigma),
-	bearing (mean.bearing(), bearing_sigma) { }
+	range_bearing_model (const position& mean,
+			double range_sigma, double bearing_sigma)
+	: range (mean.range(), range_sigma),
+	  bearing (mean.bearing(), bearing_sigma) { }
 
-    position mean () const {
-      return position_polar (range.mean(), bearing.mean()); }
+	position mean () const {
+		return position_polar (range.mean(), bearing.mean()); }
 
-    position operator() (random_source& random) const {
-      return position_polar (range(random), bearing(random));
-    }
+	position operator() (random_source& random) const {
+		return position_polar (range(random), bearing(random));
+	}
 
-    double likelihood (const position& o) const {
-      return range.likelihood (o.range())
-	* bearing.likelihood (wrap_angle (o.bearing(), bearing.mean()));
-    }
+	double likelihood (const position& o) const {
+		return range.likelihood (o.range())
+				* bearing.likelihood (wrap_angle (o.bearing(), bearing.mean()));
+	}
 
-    double log_likelihood (const position& o) const {
-      return range.log_likelihood (o.range())
-	+ bearing.log_likelihood (wrap_angle (o.bearing(), bearing.mean()));
-    }
+	double log_likelihood (const position& o) const {
+		return range.log_likelihood (o.range())
+				+ bearing.log_likelihood (wrap_angle (o.bearing(), bearing.mean()));
+	}
 
-    class builder : public std::unary_function<position, range_bearing_model> {
+	class builder : public std::unary_function<position, range_bearing_model> {
 
-      const double range_sigma, bearing_sigma;
+		const double range_sigma, bearing_sigma;
 
-    public:
+	public:
 
-      typedef position observation_type;
-      typedef range_bearing_model model_type;
+		typedef position observation_type;
+		typedef range_bearing_model model_type;
 
-      builder (double _range_sigma, double _bearing_sigma)
-	: range_sigma(_range_sigma), bearing_sigma(_bearing_sigma) { }
+		builder (double _range_sigma, double _bearing_sigma)
+		: range_sigma(_range_sigma), bearing_sigma(_bearing_sigma) { }
 
-      range_bearing_model operator() (const position& observation) const {
-	return range_bearing_model (observation, range_sigma, bearing_sigma);
-      }
+		range_bearing_model operator() (const position& observation) const {
+			return range_bearing_model (observation, range_sigma, bearing_sigma);
+		}
 
-    };
-      
+	};
 
-  };
+};
 
 
 } // namespace planar_robot
