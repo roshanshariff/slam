e95ab7e42ecfc1d876a5f8d5cbb3ef79a30b7ec3
Roshan Shariff <roshan.shariff@ualberta.ca>
2011-08-18 16:59:46 -0600

    Fixed whitespace and indentation

--- src/planar_robot/velocity_model.hpp
+++ src/planar_robot/velocity_model.hpp
@@ -15,109 +15,109 @@
 namespace planar_robot {
 
 
-  class velocity_control {
+class velocity_control {
 
-    Eigen::Vector3d control;
+	Eigen::Vector3d control;
 
-  public:
+public:
 
-    velocity_control (double v, double w, double g = 0) : control(v, w, g) { }
+	velocity_control (double v, double w, double g = 0) : control(v, w, g) { }
 
-    double v () const { return control.x(); }
-    double w () const { return control.y(); }
-    double g () const { return control.z(); }
+	double v () const { return control.x(); }
+	double w () const { return control.y(); }
+	double g () const { return control.z(); }
 
-  };
-
-
-  inline velocity_control pose_delta_to_velocity (const pose& dp, double dt = 1.0) {
-    if (dp.y() == 0) {
-      return velocity_control (dp.x()/dt, 0.0, dp.bearing()/dt);
-    }
-    else {
-      double r = 0.5 * dp.distance_squared() / dp.y();
-      double theta = std::atan2 (dp.x(), r - dp.y());
-      return velocity_control (r*theta/dt, theta/dt, wrap_angle(dp.bearing()-theta)/dt);
-    }
-  }
+};
 
 
-  inline pose velocity_to_pose_delta (const velocity_control& control, double dt = 1.0) {
-    if (control.w() == 0) {
-      return pose (control.v() * dt, 0.0, control.g() * dt);
-    }
-    else {
-      double r = control.v() / control.w();
-      double theta = control.w() * dt;
-      return pose (r*std::sin(theta), r-r*std::cos(theta), theta+control.g()*dt);
-    }
-  }
+inline velocity_control pose_delta_to_velocity (const pose& dp, double dt = 1.0) {
+	if (dp.y() == 0) {
+		return velocity_control (dp.x()/dt, 0.0, dp.bearing()/dt);
+	}
+	else {
+		double r = 0.5 * dp.distance_squared() / dp.y();
+		double theta = std::atan2 (dp.x(), r - dp.y());
+		return velocity_control (r*theta/dt, theta/dt, wrap_angle(dp.bearing()-theta)/dt);
+	}
+}
 
 
-  class velocity_model {
+inline pose velocity_to_pose_delta (const velocity_control& control, double dt = 1.0) {
+	if (control.w() == 0) {
+		return pose (control.v() * dt, 0.0, control.g() * dt);
+	}
+	else {
+		double r = control.v() / control.w();
+		double theta = control.w() * dt;
+		return pose (r*std::sin(theta), r-r*std::cos(theta), theta+control.g()*dt);
+	}
+}
 
-    normal_dist<double> v;
-    normal_dist<double> w;
-    normal_dist<double> g;
-    double dt;
 
-  public:
+class velocity_model {
 
-    typedef pose result_type;
+	normal_dist<double> v;
+	normal_dist<double> w;
+	normal_dist<double> g;
+	double dt;
 
-    velocity_model (const velocity_control& mean, double v_sigma,
-		    double w_sigma, double g_sigma, double _dt)
-      : v (mean.v(), v_sigma), w (mean.w(), w_sigma),
-	g (mean.g(), g_sigma), dt(_dt) { }
+public:
 
-    pose mean () const {
-      return velocity_to_pose_delta
-	(velocity_control (v.mean(), w.mean(), g.mean()), dt);
-    }
+	typedef pose result_type;
 
-    pose operator() (random_source& random) const {
-      return velocity_to_pose_delta
-	(velocity_control (v(random), w(random), g(random)), dt);
-    }
+	velocity_model (const velocity_control& mean, double v_sigma,
+			double w_sigma, double g_sigma, double _dt)
+	: v (mean.v(), v_sigma), w (mean.w(), w_sigma),
+	  g (mean.g(), g_sigma), dt(_dt) { }
 
-    double likelihood (const pose& p) const {
-      velocity_control control = pose_delta_to_velocity (p, dt);
-      return v.likelihood (control.v())
-	* w.likelihood (control.w())
-	* g.likelihood (control.g());
-    }
+	pose mean () const {
+		return velocity_to_pose_delta
+				(velocity_control (v.mean(), w.mean(), g.mean()), dt);
+	}
 
-    double log_likelihood (const pose& p) const {
-      velocity_control control = pose_delta_to_velocity (p, dt);
-      return v.log_likelihood (control.v())
-	+ w.log_likelihood (control.w())
-	+ g.log_likelihood (control.g());
-    }
+	pose operator() (random_source& random) const {
+		return velocity_to_pose_delta
+				(velocity_control (v(random), w(random), g(random)), dt);
+	}
 
-    class builder : public std::binary_function<double, velocity_control, velocity_model> {
+	double likelihood (const pose& p) const {
+		velocity_control control = pose_delta_to_velocity (p, dt);
+		return v.likelihood (control.v())
+				* w.likelihood (control.w())
+				* g.likelihood (control.g());
+	}
 
-      const double alpha1, alpha2, alpha3, alpha4, alpha5, alpha6;
+	double log_likelihood (const pose& p) const {
+		velocity_control control = pose_delta_to_velocity (p, dt);
+		return v.log_likelihood (control.v())
+				+ w.log_likelihood (control.w())
+				+ g.log_likelihood (control.g());
+	}
 
-    public:
+	class builder : public std::binary_function<double, velocity_control, velocity_model> {
 
-      builder (double a1, double a2, double a3, double a4, double a5, double a6)
-	: alpha1(a1), alpha2(a2), alpha3(a3), alpha4(a4), alpha5(a5), alpha6(a6) { }
+		const double alpha1, alpha2, alpha3, alpha4, alpha5, alpha6;
 
-      velocity_model operator() (double dt, const velocity_control& control) const {
+	public:
 
-	double v = std::abs (control.v());
-	double w = std::abs (control.w());
+		builder (double a1, double a2, double a3, double a4, double a5, double a6)
+		: alpha1(a1), alpha2(a2), alpha3(a3), alpha4(a4), alpha5(a5), alpha6(a6) { }
 
-	double v_sigma = alpha1 * v + alpha2 * w;
-	double w_sigma = alpha3 * v + alpha4 * w;
-	double g_sigma = alpha5 * v + alpha6 * w;
+		velocity_model operator() (double dt, const velocity_control& control) const {
 
-	return velocity_model (control, v_sigma, w_sigma, g_sigma, dt);
-      }
+			double v = std::abs (control.v());
+			double w = std::abs (control.w());
 
-    };
+			double v_sigma = alpha1 * v + alpha2 * w;
+			double w_sigma = alpha3 * v + alpha4 * w;
+			double g_sigma = alpha5 * v + alpha6 * w;
 
-  };
+			return velocity_model (control, v_sigma, w_sigma, g_sigma, dt);
+		}
+
+	};
+
+};
 
 
 } // namespace planar_robot
