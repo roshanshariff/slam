87afce8614fcd11b8710e70545eade455ea83444
Roshan Shariff <roshan.shariff@ualberta.ca>
2011-08-18 11:07:53 -0600

    Completed UKF code

--- src/utility/unscented.hpp
+++ src/utility/unscented.hpp
@@ -16,85 +16,103 @@
 
 #include "utility/multivariate.hpp"
 
-template <int N, class MinusFunc = std::minus>
+template <int N>
 class unscented_filter {
-
-	typedef multivariate_normal_dist<N> dist_type;
-
-	dist_type& state;
-
-	DifferenceFunc difference;
-
-	double alpha, kappa, beta;
 
 public:
 
-	unscented_filter (dist_type& _state) : state(_state) { }
+	typedef multivariate_normal_dist<N> dist_type;
+	typedef typename multivariate_normal_dist<N>::vector_type state_vector_type;
 
-	template <class PredictFunc>
-	void predict (const PredictFunc f) {
+private:
 
-		const double lambda = alpha*alpha*(N + kappa) - N;
+	dist_type& state;
+	StateDeltaFunc state_delta;
 
-		Eigen::Matrix<double, N, 2*N> sigmapts;
-		sigmapts.leftCols<N>() = std::sqrt(N + lambda) * state.sqrt_cov();
-		sigmapts.rightCols<N>() = -sigmapts.leftCols<N>();
-		sigmapts += state.mean().rowwise().replicate<2*N>();
+	const double lambda, eta, weight_first, weight_rest;
 
-		const Eigen::Matrix<double, N, 1> base = f(state.mean());
-		for (int i = 0; i < 2*N; ++i) sigmapts.col(i) = difference(base, f(sigmapts.col(i)));
 
-		const Eigen::Matrix<double, N, 1> base_innov = sigmapts.rowwise().sum() / (2*(N + lambda));
-		for (int i = 0; i < 2*N; ++i) sigmapts.col(i) = difference(base_innov, sigmapts.col(i));
-		sigmapts /= std::sqrt(2*(N + lambda));
+	template <int M, class TransformFunc, class DeltaFunc>
+	void apply_transform (
+			TransformFunc f, DeltaFunc delta, multivariate_normal_dist<M>& result,
+			Eigen::Matrix<double, N, M>* cross_cov = 0
+	) {
 
-		state.mean() = difference(base, base_innov);
-		state.sqrt_cov() = sigmapts.transpose().householderQr().matrixQR().topLeftCorner<N,N>().triangularView<Eigen::Upper>().transpose();
+		typedef Eigen::Matrix<double, M, 1> vector_type;
 
-		// TODO Cholesky rank-one update
-		// cholupdate(state.sqrt_cov(), std::sqrt(lambda/(L + lambda) + (1 - alpha*alpha + beta))*base_innov)
+		const vector_type base = f(state.mean());
+
+		Eigen::Matrix<double, M, 2*N> sigmapts;
+		for (int i = 0; i < N; ++i) {
+			const state_vector_type state_innov = eta * state.sqrt_cov().col(i);
+			sigmapts.col(i) = delta(base, f(state.mean() - state_innov));
+			sigmapts.col(N+i) = delta(base, f(state.mean() + state_innov));
+		}
+
+		const vector_type base_innov = weight_rest * sigmapts.rowwise().sum();
+		for (int i = 0; i < 2*N; ++i) sigmapts.col(i) = delta(base_innov, sigmapts.col(i));
+
+		result.mean() = delta(base, base_innov);
+
+		result.sqrt_cov() = sigmapts.transpose().householderQr().matrixQR()
+				.topLeftCorner<M,M>().triangularView<Eigen::Upper>().transpose();
+		result.sqrt_cov() *= std::sqrt(weight_rest);
+
+		cholesky_update(result.sqrt_cov(), base_innov, weight_first);
+
+		if (cross_cov) cross_cov->noalias() = weight_rest * eta * state.sqrt_cov()
+				* (sigmapts.rightCols<N>().transpose() - sigmapts.leftCols<N>().transpose());
+	}
+
+
+public:
+
+
+	unscented_filter (dist_type& _state, double alpha, double beta, double kappa)
+	: state(_state), lambda(alpha*alpha*(N+kappa) - N), eta(std::sqrt(N+lambda)),
+	  weight_first(lambda/(N+lambda) + 1 - alpha*alpha + beta), weight_rest(0.5 / (N+lambda))
+	{ }
+
+
+	template <class PredictFunc, class DeltaFunc = std::minus<Eigen::Matrix<double, N, 1> > >
+	void predict (PredictFunc f = PredictFunc(), DeltaFunc delta = DeltaFunc()) {
+		apply_transform(f, delta, state);
+	}
+
+
+	/* NOTE: the code below assumes that the observation has additive noise with zero mean and
+	 * covariance equal to the covariance of the sensor noise (i.e. the covariance part of the
+	 * observation parameter).
+	 */
+	template <int M, class ObsFunc, class DeltaFunc = std::minus<Eigen::Matrix<double, M, 1> > >
+	void update (
+			ObsFunc h = ObsFunc(), DeltaFunc delta = DeltaFunc(),
+			const multivariate_normal_dist<M>& observation
+	) {
+
+		multivariate_normal_dist<M> predicted;
+		Eigen::Matrix<double, N, M> cross_cov;
+
+		apply_transform(h, delta, predicted, &cross_cov)
+
+		for (int i = 0; i < M; ++i) cholesky_update(predicted.sqrt_cov(), observation.sqrt_cov().col(i));
+
+		Eigen::Matrix<double, N, M> kalman_gain =
+				predicted.sqrt_cov().triangularView<Eigen::Lower>().transpose().jacobiSvd().solve(
+						predicted.sqrt_cov().triangularView<Eigen::Lower>().jacobiSvd().solve(
+								cross_cov.transpose()
+						)
+				).transpose();
+
+		state.mean().noalias() += kalman_gain * obs_delta(observation.mean(), predicted.mean());
+
+		kalman_gain.noalias() *= predicted.sqrt_cov();
+		for (int i = 0; i < M; ++i) cholesky_downdate(state.sqrt_cov(), kalman_gain.col(i));
 
 	}
 
+
 };
-
-
-struct unscented_params {
-    double alpha, kappa, beta;
-};
-
-template <class Matrix, class Vector, class Function>
-void unscented_transform (Vector& mean, Matrix& cov, Function f, const unscented_params& p) {
-
-    const int n = mean.size();
-    const double lambda = p.alpha * p.alpha * (n + p.kappa) - n;
-
-    Matrix sqrtP = std::sqrt(n + lambda) * cov.llt().matrixU();
-
-    std::vector<Vector> sigmapts (2*n+1);
-    sigmapts[0] = f(mean);
-
-    mean = lambda * sigmapts[0];
-
-    for (int i = 1; i <= n; ++i) {
-	sigmapts[2*i-1] = f(mean + sqrtP.col(i-1));
-	sigmapts[2*i] = f(mean - sqrtP.col(i-1));
-	mean += (sigmapts[2*i-1] + sigmapts[2*i]) / 2;
-    }
-
-    mean /= n + lambda;
-
-    sigmapts[0] -= mean;
-    cov = sigmapts[0] * sigmapts[0].transpose() * (lambda + (n + lambda)*(1 - p.alpha*p.alpha + p.beta));
-
-    for (int i = 1; i <=2*n+1; ++i) {
-	sigmapts[i] -= mean;
-	cov += sigmapts[i] * sigmapts[i].transpose() / 2;
-    }
-
-    cov /= n + lambda;
-
-}
 
 
 #endif /* UNSCENTED_HPP_ */
