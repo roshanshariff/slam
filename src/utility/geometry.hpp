055334d84cfa47af9916508e585275e440fbe674
Roshan Shariff <roshan.shariff@ualberta.ca>
2011-08-23 13:07:42 -0600

    Minor changes to utility/geometry.hpp

--- src/utility/geometry.hpp
+++ src/utility/geometry.hpp
@@ -1,16 +1,15 @@
 #ifndef _UTILITY_GEOMETRY_HPP
 #define _UTILITY_GEOMETRY_HPP
 
-#include <boost/math/constants/constants.hpp>
 #include <cmath>
 
-inline double wrap_angle (double angle, double base = 0.0) {
+#include <boost/math/constants/constants.hpp>
+
+inline double wrap_angle (double angle) {
   const double PI = boost::math::constants::pi<double>();
-  angle -= base;
-  angle = fmod (angle, 2.0*PI);
+  angle = std::fmod (angle, 2.0*PI);
   if (angle < -PI) angle += 2.0*PI;
   else if (angle >= PI) angle -= 2.0*PI;
-  angle += base;
   return angle;
 }
 
