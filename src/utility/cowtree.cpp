58541c8adb57ed1b2dd1faf1d4ce78c9dfb50138
Roshan Shariff <roshan.shariff@ualberta.ca>
2011-10-10 13:03:06 -0600

    Split COW RB-tree implementation into type-independent .cpp file

--- src/utility/cowtree.cpp
+++ src/utility/cowtree.cpp
@@ -6,4 +6,51 @@
 //  Copyright 2011 University of Alberta. All rights reserved.
 //
 
-#include <iostream>
+#include <cassert>
+
+#include "utility/cowtree.hpp"
+
+
+cowtree::editor::editor (editor& parent, cowtree& subtree)
+: parent_ptr(&parent), subtree_ptr(&subtree)
+{
+    subtree.make_unique();
+}
+
+
+cowtree::editor::editor (root& tree)
+: parent_ptr(0), subtree_ptr(&tree)
+{
+    subtree().make_unique();
+    assert (subtree().is_black());
+}
+
+
+void cowtree::editor::rotate () {
+    if (is_left_child()) {
+        parent().subtree().swap(subtree().right());
+        parent().subtree().swap(subtree());
+        subtree_ptr = &parent().subtree().right();
+    }
+    else {
+        parent().subtree().swap(subtree().left());
+        parent().subtree().swap(subtree());
+        subtree_ptr = &parent().subtree().left();
+    }
+}
+
+
+cowtree::editor::~editor () {
+    if (!subtree().is_black()) {
+        if (is_root()) {
+            subtree().set_black(true);
+        }
+        else if (!parent().subtree().is_black()) {
+            if (is_left_child() != parent().is_left_child()) {
+                rotate();
+            }
+            subtree().set_black(true);
+            parent().rotate();
+        }
+    }
+}
--- src/utility/cowtree.hpp
+++ src/utility/cowtree.hpp
@@ -12,143 +12,180 @@
 #include <cassert>
 
 #include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
 #include <boost/utility.hpp>
 
-template <class T>
+
 class cowtree {
     
-    T m_data;
-    bool m_black;
-    
-    explicit cowtree (const T& data_) : m_data(data_) { }
-
 public:
     
+    bool empty () const { return node_ptr.get() == 0; }
+    
+    const cowtree& left () const;
+    const cowtree& right () const;
+    template <class T> const T& value () const;
+    
+    class root;
     class editor;
-
-    class handle {
-        
-        friend class editor;
-        
-        boost::shared_ptr<cowtree> m_node;
-        
-    public:
-        
-        handle () { }        
-        
-        bool is_empty () const { return m_node.get() == 0; }
-        
-        const handle& left () const;
-        const handle& right () const;
-        const T& data () const;
-
-        void swap (handle& other) { m_node.swap (other.m_node); }
-        
-        bool operator== (const handle& other) const { return this == &other; }
-        
-    private:
-        
-        const cowtree& node () const { assert(!is_empty()); return *m_node; }
-        cowtree& node () { assert(!is_empty()); return *m_node; }
-
-        void make_unique () {
-            if (!is_empty() && !m_node.unique()) {
-                m_node = boost::make_shared<cowtree>(node());
-            }
-        }
-
-    };
+    
+protected:
+    
+    cowtree () { }
+    cowtree (const cowtree& o) : node_ptr(o.node_ptr) { }
+    cowtree& operator= (const cowtree& o) { node_ptr = o.node_ptr; return *this; }
+    void swap (cowtree& o) { node_ptr.swap(o.node_ptr); }
     
 private:
     
-    handle m_left;
-    handle m_right;
+    class node;
+
+    const node& get_node () const { assert(!empty()); return *node_ptr; }
+    node& get_node () { assert(!empty()); return *node_ptr; }
+    
+    cowtree& left ();
+    cowtree& right ();
+    template <class T> T& value ();
+    
+    bool is_black () const;
+    void set_black (bool black);
+    void make_unique ();
+    
+    template <class T> static boost::shared_ptr<node> make_node (const T& value);
+    
+    boost::shared_ptr<node> node_ptr;
+    
+};
+
+
+class cowtree::root : public cowtree {
+public:
+    void swap (root& o) { cowtree::swap(o); }
+};
+
+
+class cowtree::node {
     
 public:
     
-    class editor : boost::noncopyable {
-        
-        editor* const m_parent;
-        handle* m_hnd;
-        
-        const handle& handle () const { assert(m_hnd); return *m_hnd; }
-        handle& handle () { assert(m_hnd); return *m_hnd; }
-        
-        bool is_black () const { return handle().is_empty() || handle().node().m_black; }
-        void set_black (bool black) { handle().node().m_black = black; }
-        
-        void rotate ();
-        
-    protected:
-        
-        editor (editor& p, handle& hnd) : m_parent(&p), m_hnd(&hnd) { }
-        
-        handle& left () { handle().make_unique(); return handle().node().m_left; }        
-        handle& right () { handle().make_unique(); return handle().node().m_right; }
+    bool black;
+    cowtree left, right;
     
-    public:
-        
-        T& data () { handle().make_unique(); return handle().node().m_data; }
-        
-        editor (handle& hnd) : m_parent(0), m_hnd(&hnd) { assert (handle().is_black()); }
-        
-        ~editor ();
-
-        bool is_root () const { return m_parent == 0; }
-        editor& parent () { assert(!is_root()); return *m_parent; }
-        
-        bool is_left_child () const { return handle() == parent().handle().left(); }
-        
-    };
+    virtual ~node() { }    
+    virtual boost::shared_ptr<node> clone () const = 0;
     
-    struct left_editor : public editor { left_editor (editor& p) : editor(p, p.left()) { } };
+    template <class T> class typed;
     
-    struct right_editor : public editor { right_editor (editor& p) : editor(p, p.right()) { } };
-        
+protected:
+    
+    node () { }
+    node (const node& o) : black(o.black), left(o.left), right(o.right) { }
+    
+private:
+    
+    node& operator= (const node& o);
+    
 };
 
 
 template <class T>
-inline const handle& cowtree<T>::handle::left () const { return node().m_left; }
+class cowtree::node::typed : public cowtree::node {
+    
+public:
+    
+    T value;
+    
+    explicit typed (const T& value) : value(value) { }
+    typed (const typed<T>& o) : node(o), value(o.value) { }
+    virtual boost::shared_ptr<node> clone () const { return boost::make_shared<typed<T> >(*this); }
+    
+private:
+    
+    typed& operator= (const typed& o);
+    
+};
 
-template <class T>
-inline const handle& cowtree<T>::handle::right () const { return node().m_right; }
 
-template <class t>
-inline const handle& cowtree<T>::handle::data () const { return node().m_data; }
+inline const cowtree& cowtree::left () const { return get_node().left; }
+inline cowtree& cowtree::left () { return get_node().left; }
 
+inline const cowtree& cowtree::right () const { return get_node().right; }
+inline cowtree& cowtree::right () { return get_node().right; }
 
-template <class T>
-void cowtree<T>::editor::rotate () {
-    if (is_left_child()) {
-        right().swap(parent().handle());
-        parent().handle().swap(handle());
-        m_hnd = &parent().right();
-    }
-    else {
-        left().swap(parent().handle());
-        parent().handle().swap(handle());
-        m_hnd = &parent().left();
-    }
+inline bool cowtree::is_black () const { return empty() || get_node().black; }
+inline void cowtree::set_black (bool black) { get_node().black = black; }
+
+inline void cowtree::make_unique () {
+    if (!empty() && !node_ptr.unique()) node_ptr = get_node().clone();
+}
+
+template <class T> inline const T& cowtree::value () const {
+    return static_cast<const node::typed<T>&>(get_node()).value;
+}
+
+template <class T> inline T& cowtree::value () {
+    return static_cast<node::typed<T>&>(get_node()).value;
+}
+
+template <class T> inline boost::shared_ptr<cowtree::node> cowtree::make_node (const T& value) {
+    return boost::make_shared<node::typed<T> >(value);
 }
 
 
-template <class T>
-cowtree<T>::editor::~inserter () {
-    if (!is_black()) {
-        if (is_root()) {
-            set_black(true);
-        }
-        else if (!parent().is_black()) {
-            if (is_left_child() != parent().is_left_child()) {
-                rotate();
-            }
-            set_black(true);
-            parent().rotate();
-        }
-    }
-}
+class cowtree::editor : boost::noncopyable {
+    
+    editor* const parent_ptr;
+    cowtree* subtree_ptr;
+    
+    cowtree& subtree () { assert(subtree_ptr != 0); return *subtree_ptr; }
+    
+    void rotate ();
+    
+    editor (editor& parent, cowtree& subtree);
+    
+public:
+    
+    editor (root& tree);
+    
+    bool is_root () const { return parent_ptr == 0; }    
+    bool is_left_child () const { return !is_root() && &subtree() == &parent().subtree().left(); }
 
+    editor& parent () { assert(!is_root()); return *parent_ptr; }
+    const editor& parent () const { assert(!is_root()); return *parent_ptr; }
+
+    const cowtree& subtree () const { assert(subtree_ptr != 0); return *subtree_ptr; }
+    
+    template <class T> T& value () { return subtree().value<T>(); }    
+    template <class T> void insert (const T& value);
+    
+    ~editor ();
+    
+    class left;
+    class right;
+    
+};
+
+
+class cowtree::editor::left : public cowtree::editor {
+    left (const left&);
+    left& operator= (const left&);
+public:
+    left (editor& parent) : editor(parent, parent.subtree().left()) { }
+};
+
+
+class cowtree::editor::right : public cowtree::editor {
+    right (const right&);
+    right& operator= (const right&);
+public:
+    right (editor& parent) : editor(parent, parent.subtree().right()) { }
+};
+
+
+template <class T> void cowtree::editor::insert (const T& value) {
+    assert(subtree().empty());
+    subtree().node_ptr = cowtree::make_node(value);
+}
 
 
 #endif
