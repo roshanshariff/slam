55fbb57779b4b3ac2cc5f65155425f28a9591b49
Roshan Shariff <roshan.shariff@ualberta.ca>
2012-01-27 16:41:26 -0700

    Moved remember_option from utility/options.hpp to utility/utility.hpp

--- src/utility/utility.hpp
+++ src/utility/utility.hpp
@@ -10,13 +10,17 @@
 #define slam_utility_hpp
 
 #include <cstdio>
+#include <string>
 #include <utility>
 
 #include <boost/shared_ptr.hpp>
+#include <boost/program_options.hpp>
+
 
 boost::shared_ptr<FILE> open_file (const char* filename, const char* mode);
 
 boost::shared_ptr<FILE> open_process (const char* command, const char* mode);
+
 
 template <class U, class Compare = std::less<U> >
 class pair_compare_first {
@@ -27,5 +31,16 @@ public:
         return cmp (a.first, b.first);
     }
 };
+
+
+template <class T>
+T remember_option (boost::program_options::variables_map& options, const std::string& key, T default_value) {
+    if (options[key].empty()) {
+        options.insert (std::make_pair (key, boost::program_options::variable_value (default_value, false)));
+        return default_value;
+    }
+    else return options[key].as<T>();
+}
+
 
 #endif
